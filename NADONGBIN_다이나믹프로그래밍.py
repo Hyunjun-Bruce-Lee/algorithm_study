d3474e30de2a48bd09852a64f9f3a50047545aa5
Hyunjun-Bruce-Lee <60064604+Hyunjun-Bruce-Lee@users.noreply.github.com>
2021-12-07 09:48:47 +0900

    ADD new case(#4)

--- NADONGBIN_다이나믹프로그래밍.py
+++ NADONGBIN_다이나믹프로그래밍.py
@@ -31,7 +31,6 @@ memo[num]
 
 
 #### 3
-
 n, m = 3, 17
 coins = [2,3,5]
 
@@ -47,3 +46,29 @@ if memo[m] == 10001:
     print(-1)
 else:
     print(memo[m])
+
+    
+#### 4
+n,m = 3,4
+mine_info = [1,3,3,2,2,1,4,1,0,6,4,7]
+
+n,m = 4,4
+mine_info = [1,3,1,5,2,2,4,1,5,0,2,3,0,6,1,2]
+
+
+mine = [mine_info[i:i+4] for i in range(0,len(mine_info),m)]
+gold_gain = [[0 for i in range(m)] for i in range(n)]
+for i in range(n):
+    gold_gain[i][0] = mine[i][0]
+
+for i in range(1,m):
+    for j in range(n):
+        if j - 1 < 0:
+            gold_gain[j][i] = max(gold_gain[j][i-1]+mine[j][i],gold_gain[j+1][i-1]+mine[j][i])
+        elif j + 1 >= n:
+            gold_gain[j][i] = max(gold_gain[j][i-1]+mine[j][i],gold_gain[j-1][i-1]+mine[j][i])
+        else:
+            gold_gain[j][i] = max(gold_gain[j][i-1]+mine[j][i],gold_gain[j-1][i-1]+mine[j][i],gold_gain[j+1][i-1]+mine[j][i])
+
+temp = [gold_gain[i][m-1] for i in range(n)]    
+max(temp)
