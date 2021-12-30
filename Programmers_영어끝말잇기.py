1cb80af94303194a17a6324ed72b5184c7183c8f
Hyunjun-Bruce-Lee <60064604+Hyunjun-Bruce-Lee@users.noreply.github.com>
2021-12-30 15:31:21 +0900

    Update and rename Programmers_영어끝말잇기(미결).py to Programmers_영어끝말잇기.py

--- Programmers_영어끝말잇기.py
+++ Programmers_영어끝말잇기.py
@@ -15,3 +15,20 @@ def solution(n, words):
         else:
             spoken_words.append(next_word)
     return [0,0]
+
+
+### return [n - (count % n), math.ceil(count / n)] 부분에서 n - (count % n) 부분이 문제
+### 다음과 같이 수정 후 통과
+
+def solution(n, words):
+    words = list(reversed(words))
+    spoken_words = [words.pop()]
+    count = 1
+    while words:
+        next_word = words.pop()
+        count += 1
+        if (next_word[0] != spoken_words[-1][-1]) | (next_word in spoken_words):
+            return [n if count % n == 0 else count % n, math.ceil(count / n)]
+        else:
+            spoken_words.append(next_word)
+    return [0,0]
